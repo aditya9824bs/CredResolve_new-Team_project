9d09d2784827f66d995f8d46dde1713d8b9adba2
BIBEK7932 <bibekdas7932@gmail.com>
2025-03-21 13:07:57 +0530

    backend initilized

---
+++ CredEasy/src/main/java/com/credResolve/CredEasy/CredEasyApplication.java
@@ -0,0 +1,13 @@
+package com.credResolve.CredEasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CredEasyApplication.class, args);
+	}
+
+}
---
+++ CredEasy/src/test/java/com/credResolve/CredEasy/CredEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.credResolve.CredEasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredEasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
